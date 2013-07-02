13ad7b820562c5fcbd576e007e778a6f48a567bb
Ki-Hwan Lee <kiban18@gmail.com>
2013-07-02 15:53:26 +0900

    add

--- install.sh
+++ install.sh
@@ -14,13 +14,13 @@ echo "OS:$OS for debug"
 if [ -f .profile ]; then
     mv .profile .profile.bak
 fi
-ln -s .envsetup.$OS/_profile .profile
+cp .envsetup.$OS/_profile .profile
 echo "refresh .profile"
 
 if [ -f .vimrc ]; then
     mv .vimrc .vimrc.bak
 fi
-ln -s .envsetup.$OS/_vimrc .vimrc
+cp .envsetup.$OS/_vimrc .vimrc
 echo "refresh .vimrc"
 
 if [ -f .gitconfig ]; then
