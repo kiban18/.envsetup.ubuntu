0e902e4678ceb188ccfa92729c1f0c883105f3ca
Ki-Hwan Lee <kiban18@gmail.com>
2013-07-03 09:51:56 +0900

    ...

--- install.sh
+++ install.sh
@@ -7,26 +7,26 @@ case $OSTYPE in
     linux*)  OS=ubuntu;;
 esac
 
-echo "[DEBUG] OS:$OS HOSTNAME:$HOSTNAME PWD:$PWD"
+echo "  [DEBUG] OS:$OS HOSTNAME:$HOSTNAME PWD:$PWD"
 
 if [ -f .profile ]; then
     cat .profile > .profile.bak
     rm .profile
 fi
 ln -s .envsetup.$OS/_profile .profile
-echo "  refresh .profile"
+echo "    refresh .profile"
 
 if [ -f .vimrc ]; then
     cat .vimrc > .vimrc.bak
     rm .vimrc
 fi
 ln -s .envsetup.$OS/_vimrc .vimrc
-echo "  refresh .vimrc"
+echo "    refresh .vimrc"
 
 if [ -f .gitconfig ]; then
     mv .gitconfig .gitconfig.bak
 fi
 cp .envsetup.$OS/_gitconfig .gitconfig
-echo "  refresh .gitconfig"
+echo "    refresh .gitconfig"
 
 popd
