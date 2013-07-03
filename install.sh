531eee6085c1f9a9abddf6d7b40c6ef12be68ca5
Ki-Hwan Lee <kiban18@gmail.com>
2013-07-03 09:42:42 +0900

    ...

--- install.sh
+++ install.sh
@@ -16,19 +16,19 @@ if [ -f .profile ]; then
     rm .profile
 fi
 ln -s .envsetup.$OS/_profile .profile
-echo "refresh .profile"
+echo "  refresh .profile"
 
 if [ -f .vimrc ]; then
     cat .vimrc > .vimrc.bak
     rm .vimrc
 fi
 ln -s .envsetup.$OS/_vimrc .vimrc
-echo "refresh .vimrc"
+echo "  refresh .vimrc"
 
 if [ -f .gitconfig ]; then
     mv .gitconfig .gitconfig.bak
 fi
 cp .envsetup.$OS/_gitconfig .gitconfig
-echo "refresh .gitconfig"
+echo "  refresh .gitconfig"
 
 popd
