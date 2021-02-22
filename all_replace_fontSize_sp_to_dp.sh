81ee35b4743892cf8fd2bebfb97f3a83a6a6d143
levi <levi@crazyupinc.com>
2021-02-22 21:53:49 +0900

    ..

---
+++ all_replace_fontSize_sp_to_dp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FROM=$1
+TO=$2
+
+read -p "${FROM} --> ${TO}, Press [Enter] ..."
+
+#find . -type f \( ! -iname "tags" -or ! -iname "cscope.*" \) -exec sed -i "s/\<${FROM}\>/${TO}/g" {} \;
+find . -type f \( ! -iname "tags" -or ! -iname "cscope.*" \) -exec sed -i "s/${FROM}/${TO}/g" {} \;
+#find . -name .git -prune -o -name tags -prune -o -name 'cscope.*' -prune -type f -exec sed -i "s/\<${FROM}\>/${TO}/g" {} \;
