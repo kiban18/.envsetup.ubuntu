d4d3149d357a6d174693787b106c9c8a444003b9
khlee <khlee@crazyupinc.com>
2022-04-06 22:35:28 +0900

    ..

--- lint-unicus-fota-connect.sh
+++ lint-unicus-fota-connect.sh
@@ -2,4 +2,3 @@
 
 pushd /work/source/marklink/unicus/marklink-unicus/sshaws
 ssh -i "cougar.pem" ubuntu@ec2-13-125-100-67.ap-northeast-2.compute.amazonaws.com
-echo "cd ~/marklink-unicus/unicus-server"
